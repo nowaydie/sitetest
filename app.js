ea3c0d507cf5ae52e415b0a297fa145b85665021
Misha Bogdanov <67557582+nowaydie@users.noreply.github.com>
2022-12-15 22:59:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+let tg = window.Telegram.WebApp;
+
+let height = document.documentElement.scrollHeight;
+let width = document.documentElement.scrollWidth;
+
+tg.expand();
+
+
+$("#slider2").ionRangeSlider({
+    type: "double",
+    skin:"round",
+    min: 0,
+    max: 60000000,
+    from: 0,
+    to: 60000000,
+    step: 500000,
+	postfix: '$',
+	hide_min_max: true,
+    onFinish: function (data) {
+    		cost_from=data.from;
+    		cost_to=data.to;
+    },
+
+});
+
+
+$("#slider1").ionRangeSlider({
+    type: "double",
+    skin:"round",
+    min: 0,
+    max: 60000000,
+    from: 0,
+    to: 60000000,
+    step: 500000,
+	postfix: '$',
+	hide_min_max: true,
+    onFinish: function (data) {
+    		cost_from=data.from;
+    		cost_to=data.to;
+    },
+
+});
+
+
+active = 1;
+$(".inner").appendTo($(".container"));
+let p = $(".inner2").detach();
+
+
+function SendData(){
+		let data='';
+		if (document.getElementByName("fromc").checked==1) data+='new_building ';
+		if (document.getElementByName("to1").checked==1) data+='from_the_owner ';
+		
+		if (document.getElementByName("start").checked==1) data+='Dubai ';
+
+		if (document.getElementByName("end").checked==1) data+='apartment ';
+		if (document.getElementByName("adult").checked==1) data+='townhouse ';
+		if (document.getElementByName("child").checked!=1) data+='villa ';
+
+
+		if (document.getElementByName("types").checked==1) data+='studio ';
+
+		console.log(data);
+		tg.sendData(data);
+}
+
---
+++ script.js
@@ -0,0 +1,5 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "red";
+tg.MainButton.color = "green";
+tg.MainButton.setText("main_btn");
