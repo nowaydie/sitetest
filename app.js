43f9b499c2397a7b6051ea9c6ec61a38c57aee3f
Misha Bogdanov <67557582+nowaydie@users.noreply.github.com>
2022-12-16 00:21:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let p = $(".inner2").detach();
 
 function SendData(){
 		let data='12345';
-// 		if (document.getElementByName("fromc").checked==1) data+='new_building ';
+		if (document.getElementByName("fromc")) data+=document.getElementByName("fromc"));
 // 		if (document.getElementByName("to1").checked==1) data+='from_the_owner ';
 		
 // 		if (document.getElementByName("start").checked==1) data+='Dubai ';
