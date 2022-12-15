85d9ec21dbf23a33a2e3c02907d07e7d90834eda
Misha Bogdanov <67557582+nowaydie@users.noreply.github.com>
2022-12-16 00:09:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ let p = $(".inner2").detach();
 
 function SendData(){
 		let data='12345';
-		if (document.getElementByName("fromc").checked==1) data+='new_building ';
-		if (document.getElementByName("to1").checked==1) data+='from_the_owner ';
+// 		if (document.getElementByName("fromc").checked==1) data+='new_building ';
+// 		if (document.getElementByName("to1").checked==1) data+='from_the_owner ';
 		
-		if (document.getElementByName("start").checked==1) data+='Dubai ';
+// 		if (document.getElementByName("start").checked==1) data+='Dubai ';
 
-		if (document.getElementByName("end").checked==1) data+='apartment ';
-		if (document.getElementByName("adult").checked==1) data+='townhouse ';
-		if (document.getElementByName("child").checked!=1) data+='villa ';
+// 		if (document.getElementByName("end").checked==1) data+='apartment ';
+// 		if (document.getElementByName("adult").checked==1) data+='townhouse ';
+// 		if (document.getElementByName("child").checked!=1) data+='villa ';
 
 
-		if (document.getElementByName("").checked==1) data+='studio ';
+// 		if (document.getElementByName("").checked==1) data+='studio ';
 
 		console.log(data);
 		tg.sendData(data);
